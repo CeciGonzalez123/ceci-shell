cf00c92c988c92b8c22b525ce06def50fca8deaf
CeciGonzalez123 <89223214+CeciGonzalez123@users.noreply.github.com>
2023-02-02 22:33:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import util
 import comandos
 from colorama import init, Fore, Back
+import subprocess
 
 init(autoreset=True)
 
 def ayuda():
     print(util.negrita(Fore.YELLOW+"COMANDOS DISPONIBLES"))
-    print(f"\tcopiar\t\t{Fore.BLUE}[origen_1] [origen_2] [origen_n] {Fore.GREEN}[destino] {Fore.RESET} ==> Copia archivo(s) o carpeta(s)")
-    print(f"\tmover\t\t{Fore.BLUE}[origen_1] [origen_2] [origen_n] {Fore.GREEN}[destino] {Fore.RESET} ==> Mueve archivo o carpeta")
-    print(f"\trenombrar\t{Fore.BLUE}[arcivo] {Fore.GREEN}[nuevo_nombre]\t {Fore.RESET} ==> Renombra archivo")
-    print(f"\tlistar\t\t{Fore.BLUE}[ruta] (opcional) \t {Fore.RESET} ==> Lista archivos")
-    print(f"\tcreadir\t\t{Fore.BLUE}[dir_1] [dir_2] [dir_3]\t{Fore.RESET}  ==> Crea directorios")
-    print(f"\tir\t\t{Fore.BLUE}[directotio]\t{Fore.RESET}  ==> Carmbia a directorio")
-    print(f"\tpermiso\t\t{Fore.BLUE}[recurso] {Fore.GREEN}[permiso] {Fore.RESET} ==> Asigna permisos a archivo o carpeta")
-    print(f"\tpropiedad\t{Fore.BLUE}[recurso_1] [recurso_2] [recurso_n] {Fore.RESET} ==> Cambia propietario de archivo(s) o carpeta(s)")
-    print(f"\truta ==> Muestra ruta actual")
-    print(f"\thistorial ==> Muestra el historial de comandos ejecutados")
-    print(f"\tbuscar\t{Fore.BLUE}[arcivo] {Fore.GREEN}[cadena] {Fore.RESET} ==> Buscar cadena en archivo de texto")
-    print(f"\tclave ==> Cambiar contraseña")
-    print(f"\tsalir ==> Salir de la ceci-shell")
+    print(f"  copiar {Fore.BLUE}[origen_1] [origen_2] [origen_n] {Fore.GREEN}[destino] {Fore.RESET} ==> Copia archivo(s) o carpeta(s)")
+    print(f"  mover {Fore.BLUE}[origen_1] [origen_2] [origen_n] {Fore.GREEN}[destino] {Fore.RESET} ==> Mueve archivo o carpeta")
+    print(f"  renombrar {Fore.BLUE}[arcivo] {Fore.GREEN}[nuevo_nombre] {Fore.RESET} ==> Renombra archivo")
+    print(f"  listar {Fore.BLUE}[ruta] (opcional) {Fore.RESET} ==> Lista archivos")
+    print(f"  creadir {Fore.BLUE}[dir_1] [dir_2] [dir_3] {Fore.RESET}  ==> Crea directorios")
+    print(f"  ir {Fore.BLUE}[directotio] {Fore.RESET}  ==> Carmbia a directorio")
+    print(f"  permiso {Fore.BLUE}[recurso] {Fore.GREEN}[permiso] {Fore.RESET} ==> Asigna permisos a archivo o carpeta")
+    print(f"  propiedad {Fore.BLUE}[recurso_1] [recurso_2] [recurso_n] {Fore.RESET} ==> Cambia propietario de archivo(s) o carpeta(s)")
+    print(f"  ruta ==> Muestra ruta actual")
+    print(f"  historial ==> Muestra el historial de comandos ejecutados")
+    print(f"  buscar {Fore.BLUE}[arcivo] {Fore.GREEN}[cadena] {Fore.RESET} ==> Buscar cadena en archivo de texto")
+    print(f"  ejecutar {Fore.BLUE}[comando] {Fore.RESET} ==> Ejecutar otros comandos")
+    print(f"  stop ==> stop")
+    print(f"  clave ==> Cambiar contraseña")
+    print(f"  salir ==> Salir de la ceci-shell")
 
 def main():
     """Programa principal que ejecuta los comandos"""
     print(util.negrita(Back.GREEN+Fore.WHITE+" ***** BIENVENID@ A LA ceci-shell EN PYTHON *****"))
+    history = []
     while True:
         cmd = input("ceci-shell> ").strip().split()
+        history.append(' '.join(cmd))
         if not cmd:
             continue
         if cmd[0].lower() == "salir":
@@ -77,12 +82,17 @@ def main():
         elif cmd[0].lower() == "ruta":
             comandos.ruta()
         elif cmd[0].lower() == "historial":
-            comandos.historial()
+            util.respuesta((", ").join(history))
         elif cmd[0].lower() == "buscar":
             if len(cmd) != 3:
                 print(f"Uso: buscar {Fore.BLUE} [archivo] {Fore.GREEN} [cadena]")
             else:
                 comandos.buscar(cmd[1], cmd[2])
+        elif cmd[0].lower() == "ejecutar":
+            if len(cmd) > 1:
+                comandos.ejecutar(cmd[1:])
+            else:
+                print(f"Uso: ejecutar {Fore.BLUE} [comando]")
         elif cmd[0].lower() == "ayuda":
             ayuda()
         else:
