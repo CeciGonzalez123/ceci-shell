9e599603f48e823e6da6a28669eede031b747b34
CeciGonzalez123 <89223214+CeciGonzalez123@users.noreply.github.com>
2023-02-03 15:43:40 -0300

    Create ftp.py

---
+++ ftp.py
@@ -0,0 +1,26 @@
+import util
+import logger
+import ftplib
+from pathlib import Path
+
+def transferir(archivo):
+    # Solicita servidor ftp y credenciales para conexión
+    host = input("Ingrese IP o nombre del servidor ftp: ")
+    usuario = input("Ingrese el nombre de usuario: ")
+    clave = input("Ingrese password: ")
+
+    try:
+        ftp = ftplib.FTP(host)
+        ftp.login(usuario, clave)
+        ftp.cwd('files')
+        with open(archivo, 'rb') as f:
+            ftp.storbinary('STOR ' + archivo, f)
+        print(ftp.retrlines('LIST'))
+        ftp.quit()
+        log = f"Archivo {archivo} transferido exitosamente a {host}"        
+        util.respuesta(log)
+        logger.log(str(log), 2)
+    except Exception as e:
+        log = f"Error al transferir el archivo {archivo} a {host}: {e}"
+        util.respuesta(log, "error")
+        logger.log(str(e), 3)
