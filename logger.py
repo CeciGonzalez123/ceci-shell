4237d8940b339895f8e2ed8db92af0c9896a9330
CeciGonzalez123 <89223214+CeciGonzalez123@users.noreply.github.com>
2023-02-04 11:37:52 -0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,13 +1,20 @@
 import logging
 
 def log(message, log_type):
+    """
+    Registra mensage en logs segun el tipo indicado
+    """
+
+    # Crea instancia del objeto logging
     logger = logging.getLogger(log_type)
     logger.setLevel(logging.INFO)
 
+    # Define tipo de log
     log_file = "/var/log/shell/{}.log".format(log_type)
     handler = logging.FileHandler(log_file)
     handler.setLevel(logging.INFO)
 
+    # Estable formato del log
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
 
