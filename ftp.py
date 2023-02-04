8658f22b6b1e77ac29d55a90754ffd4d099e0481
CeciGonzalez123 <89223214+CeciGonzalez123@users.noreply.github.com>
2023-02-04 11:36:53 -0300

    Update ftp.py

--- ftp.py
+++ ftp.py
@@ -1,20 +1,30 @@
 import util
 import logger
 import ftplib
+import util
 from pathlib import Path
 
 def transferir(archivo):
+    """
+    Transfiere archivo via ftp
+    """
+
     # Solicita servidor ftp y credenciales para conexión
     host = input("Ingrese IP o nombre del servidor ftp: ")
     usuario = input("Ingrese el nombre de usuario: ")
     clave = input("Ingrese password: ")
 
     try:
+        # Crea instancia de objeto ftp
         ftp = ftplib.FTP(host)
+        # Autentica con credenciales de la cuenta ftp
         ftp.login(usuario, clave)
+        # Se mueve a carpeta files
         ftp.cwd('files')
+        # Lee y transfiere contenido binario
         with open(archivo, 'rb') as f:
             ftp.storbinary('STOR ' + archivo, f)
+        # Imprime contenido de carpeta files
         print(ftp.retrlines('LIST'))
         ftp.quit()
         log = f"Archivo {archivo} transferido exitosamente a {host}"        
